e30f64d4610b6fe1af0563b2b2a5584cb8346a93
Christian <37517470+Draguwro@users.noreply.github.com>
2021-02-04 09:11:22 -0500

    Remove back button transition
    Closes #9
    Decided to just remove it #11

--- src/comps/Article.js
+++ src/comps/Article.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/article.css";
-import {Link} from "react-router-dom";
+import {withRouter} from "react-router-dom";
 import parse, {domToReact} from "html-react-parser";
 import { v4 as uuidv4 } from "uuid";
 
@@ -9,17 +9,41 @@ class Article extends React.Component {
   constructor(props) {
     super(props);
     this.articleRef = React.createRef();
+    this.metadataRef = React.createRef();
+    this.vanishArticle = this.vanishArticle.bind(this);
   };
   
   componentDidMount() {
-    setTimeout(() => this.articleRef.current.classList.toggle("visible"), 300);
-  }
+    /*
+    // This makes the page flash on back button
+    const {history} = this.props;
+    window.onpopstate = (event) => {
+      event.preventDefault();
+      if (this.articleRef.current.classList.contains("visible")) {
+        this.articleRef.current.classList.remove("visible");
+      };
+    };*/ 
+    setTimeout(() => this.articleRef.current.classList.add("visible"), 300);
+  };
+  
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.articleRef.current.classList.add("visible");
+    }, 300);
+  };
+  
+  vanishArticle(event) {
+    event.preventDefault();
+    this.articleRef.current.classList.remove("visible");
+    
+    const {history} = this.props;
+    setTimeout(() => history.push(event.target.getAttribute("href")), 300);
+  };
   
   render() {
-    
     return (
       <article ref={this.articleRef}>
-        <div id="article-metadata">
+        <div id="article-metadata" ref={this.metadataRef}>
           <h1 id="article-name">{this.props.articleName}</h1>
           <div id="article-contributors">
             <div id="article-contributor-bubbles">
@@ -33,17 +57,17 @@ class Article extends React.Component {
           </div>
         </div>
         <div id="article-content">{
-          this.props.content.map((element) => {
+          this.props.exists ? this.props.content.map((element) => {
             return parse("<" + element[0] + " key={" + uuidv4() + ">" + element[1] + "</" + element[0] + ">", {
               replace: (domNode) => {
-                return domNode.children ? (domNode.name === "a" ? <Link to={domNode.attribs.href}>{domToReact(domNode.children)}</Link> : domNode) : undefined;
+                return domNode.children ? (domNode.name === "a" ? <a onClick={this.vanishArticle} href={domNode.attribs.href}>{domToReact(domNode.children)}</a> : domNode) : undefined;
               }
             })
-          })
+          }) : <div>This article doesn't exist!</div>
         }</div>
       </article>
     );
   };
 };
 
-export default Article;
+export default withRouter(Article);
