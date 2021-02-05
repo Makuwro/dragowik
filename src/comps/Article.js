3a4f52393215469a98b79bcfcff420c3a80a66c4
Christian <37517470+Draguwro@users.noreply.github.com>
2021-02-04 23:44:56 -0500

    Add redirect support

--- src/comps/Article.js
+++ src/comps/Article.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "../styles/article.css";
-import {withRouter} from "react-router-dom";
+import {withRouter, Redirect} from "react-router-dom";
 import parse, {domToReact} from "html-react-parser";
 import { v4 as uuidv4 } from "uuid";
+
+const RedirectRegex = /#REDIRECT \[\[(\w+)\]\]/g;
+const ArticleNameRegex = /\/wiki\/article\/(\w+)/g
 
 class Article extends React.Component {
   
@@ -11,6 +14,18 @@ class Article extends React.Component {
     this.articleRef = React.createRef();
     this.metadataRef = React.createRef();
     this.vanishArticle = this.vanishArticle.bind(this);
+    this.redirectArticle = this.redirectArticle.bind(this);
+    var firstElement = this.props.content ? this.props.content[0] : undefined;
+    var redirectArticleName;
+    if (firstElement) {
+      if (firstElement[0] === "div") {
+        var redirectArticle = [...firstElement[1].matchAll(RedirectRegex)][0];
+        redirectArticleName = redirectArticle ? redirectArticle[1] : undefined;
+      };
+    };
+    this.state = {
+      redirectArticleName: redirectArticleName,
+    }
   };
   
   componentDidMount() {
@@ -27,9 +42,15 @@ class Article extends React.Component {
   };
   
   componentDidUpdate() {
-    setTimeout(() => {
-      this.articleRef.current.classList.add("visible");
-    }, 300);
+    if (this.props.location.redirectedFrom && this.state.redirectArticleName) {
+      this.setState({redirectArticleName: undefined});
+    };
+    
+    if (!this.state.redirectArticleName) {
+      setTimeout(() => {
+        this.articleRef.current.classList.add("visible");
+      }, 300);
+    };
   };
   
   vanishArticle(event) {
@@ -40,33 +61,52 @@ class Article extends React.Component {
     setTimeout(() => history.push(event.target.getAttribute("href")), 300);
   };
   
+  redirectArticle() {
+    console.log("Redirecting from " + this.props.articleName + " to " + this.state.redirectArticleName);
+    return <Redirect to={{
+      pathname: "/wiki/article/" + this.state.redirectArticleName,
+      redirectedFrom: this.props.articleName
+    }} />
+  };
+  
   render() {
-    return (
-      <article ref={this.articleRef}>
-        <div id="article-metadata" ref={this.metadataRef}>
-          <h1 id="article-name">{this.props.articleName}</h1>
-          <div id="article-contributors">
-            <div id="article-contributor-bubbles">
-              <span></span>
-              <span></span>
-              <span></span>
+    if (this.state.redirectArticleName && !this.props.location.redirectedFrom) {
+      return (this.redirectArticle());
+    } else {
+      return (
+        <article ref={this.articleRef}>
+          <div id="article-metadata" ref={this.metadataRef}>
+            <h1 id="article-name">{this.props.articleName}</h1>
+            <div id="article-contributors">
+              <div id="article-contributor-bubbles">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+              <span id="article-contributors-amount">0 contributors</span>
+              <span id="article-contribs-divider">•</span>
+              <span id="article-update-time">Just updated</span>
             </div>
-            <span id="article-contributors-amount">0 contributors</span>
-            <span id="article-contribs-divider">•</span>
-            <span id="article-update-time">Just updated</span>
           </div>
-        </div>
-        <div id="article-content">{
-          this.props.exists ? this.props.content.map((element) => {
-            return parse("<" + element[0] + " key={" + uuidv4() + ">" + element[1] + "</" + element[0] + ">", {
-              replace: (domNode) => {
-                return domNode.children ? (domNode.name === "a" ? <a onClick={this.vanishArticle} href={domNode.attribs.href}>{domToReact(domNode.children)}</a> : domNode) : undefined;
-              }
-            })
-          }) : <div>This article doesn't exist!</div>
-        }</div>
-      </article>
-    );
+          <div id="article-content">{
+            this.props.exists ? this.props.content.map((element) => {
+              
+              return parse("<" + element[0] + " key={" + uuidv4() + ">" + element[1] + "</" + element[0] + ">", {
+                replace: (domNode) => {
+                  if (domNode.children) {
+                    if (domNode.name === "a") {
+                      var articleExists = this.state.nonExistentArticles[domNode.attribs.href] ? false : true; // returns unresolved promise
+                      return <a onClick={this.vanishArticle} href={domNode.attribs.href} title={!articleExists ? "This article doesn't exist! Why don't we change that?" : "oops"} className={!articleExists ? "article-link-invalid" : undefined}>{domToReact(domNode.children)}</a>;
+                    };
+                    return domNode;
+                  };
+                }
+              })
+            }) : <div>This article doesn't exist!</div>
+          }</div>
+        </article>
+      );
+    }
   };
 };
 
